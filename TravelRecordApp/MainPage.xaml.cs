9da085714a06430462992e9b6b870d92fe39611c
ioanaS16 <stefanioana16@gmail.com>
2021-10-23 17:06:11 +0300

    Add project files.

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text); //returneaza true daca elementul e gol
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+            
+            if( isEmailEmpty || isPasswordEmpty ) //doar daca una sau doua sunt adevarate
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
